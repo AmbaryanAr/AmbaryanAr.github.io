fd1b8e6ebadcbbe897956819056419c2f8cf3e45
AmbaryanAr <97750684+AmbaryanAr@users.noreply.github.com>
2022-03-13 22:01:24 +0300

    Update app.js

--- TestGame/app.js
+++ TestGame/app.js
@@ -10,7 +10,7 @@ VK.init(function() {
 
 function init(){
 	VK.api("users.get", {"fields": "photo_50,first_name,last_name", "v":"5.73"}, function (data) {
-		console.log(data);
+		console.log(data.response);
 		//user_name = data.response.first_name + " " + data.response.last_name;
 		//user_icon = data.response.photo_50;
 		//_data = data
