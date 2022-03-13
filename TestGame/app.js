8fa29ad4dbce2a5d2c540708519fed9177d969e5
AmbaryanAr <97750684+AmbaryanAr@users.noreply.github.com>
2022-03-13 21:21:56 +0300

    0.3.2

--- TestGame/app.js
+++ TestGame/app.js
@@ -1,8 +1,26 @@
 var user_name = ""
+var user_icon = ""
 
-setTimeout(()=>{
-	user_name="Петр";
-},500);
+VK.init(function() {
+    init();
+}, function() {
+	alert("error init api vk");
+}, '5.131');
+
+function init(){
+	VK.api("user.get", {"fields": "photo_50,first_name,last_name", "v":"5.73"}, function (data) {
+		user_name = data.response[0].first_name + " " + data.response[0].last_name;
+		user_icon = data.response[0].photo_50;
+		
+		var user_data = documentcreateElement("div");
+		user_data.className = "user_data";
+		user_data.innerHTML = `
+			<img src='` + user_icon + `'>
+			<span>` + user_name + `</span>
+		`;
+		document.body.appendChild(user_data);
+	});
+};
 
 function publich(code, value){
 	alert("my data " + code + " and " + value);
